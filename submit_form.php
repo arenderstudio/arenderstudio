f1587410f7ac9eb3960e1c26502828f25264fa7d
arenderstudio <connect.arenderstudio@gmail.com>
2025-10-14 16:11:29 +0530

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -53,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
             $mail->Username = 'aangadranjitsingh@gmail.com';
-            $mail->Password = 'pgzo gfwg uvnm dony';
+            $mail->Password = 'rgzn qxbs oibz hrgo';
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
 
